d0d928e077245593960fe71acc0ac2ce51891d5a
Syotakokichi <kokichiyam@gmail.com>
2023-07-22 18:31:54 +0900

    ルーティング変更 & ファイル整理

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// App.tsx
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PatientsList from './components/pages/PatientsList';
 import PatientCreate from './components/pages/PatientCreate';
 import PatientUpdate from './components/pages/PatientUpdate';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/patients" element={<PatientsList />} />
-        <Route path="/patients/new" element={<PatientCreate />} />
-        <Route path="/patients/:id/update" element={<PatientUpdate />} />
-      </Routes>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/patients">Go to Patients List</Link>
+          </nav>
+          <Routes>
+            <Route path="/patients" element={<PatientsList />} />
+            <Route path="/patients/new" element={<PatientCreate />} />
+            <Route path="/patients/:id/update" element={<PatientUpdate />} />
+          </Routes>
+        </div>
+      </Router>
+    </RecoilRoot>  
   );
-};
+}
 
 export default App;
--- frontend/src/components/pages/PatientUpdate.tsx
+++ frontend/src/components/pages/PatientUpdate.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+// PatientUpdate.tsx
+import { useNavigate, useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { patientListState } from '../../state/patients';
+import { Patient } from '../../state/patients';
+import PatientForm from '../../shared/PatientForm';
 
-const PatientUpdate = () => {
-  return <div>Patient Update Page</div>;
+const PatientUpdate: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patients, setPatients] = useRecoilState(patientListState);
+  const patientIndex = patients.findIndex(patient => patient.id === Number(id));
+  const patient = patients[patientIndex];
+
+  const navigate = useNavigate();
+
+  const onSubmit = (data: Patient) => {
+    const updatedPatients = [...patients];
+    updatedPatients[patientIndex] = { ...patient, ...data };
+    setPatients(updatedPatients);
+    navigate("/patients");
+  };
+
+  return patient ? (
+    <div>
+      <h1>Edit Patient</h1>
+      <PatientForm defaultValues={patient} onSubmit={onSubmit} />
+    </div>
+  ) : null;
 };
 
 export default PatientUpdate;
