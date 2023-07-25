51e1a60ecd1ccf38c7cd83c78e4183b1bd9bc30e
Syotakokichi <kokichiyam@gmail.com>
2023-07-25 16:45:48 +0900

    App.tsx変更

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PatientsList from './components/pages/PatientsList';
 import PatientCreate from './components/pages/PatientCreate';
 import PatientUpdate from './components/pages/PatientUpdate';
-import { AppContainer, Nav } from './styles/AppStyles';
+import { AppContainer} from './styles/AppStyles';
 
 const App = () => {
   return (
@@ -12,6 +12,7 @@ const App = () => {
       <Router>
         <AppContainer>
           <Routes>
+            <Route path="/" element={<PatientsList />} /> 
             <Route path="/patients" element={<PatientsList />} />
             <Route path="/patients/new" element={<PatientCreate />} />
             <Route path="/patients/:id/update" element={<PatientUpdate />} />
