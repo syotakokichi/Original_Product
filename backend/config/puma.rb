277fe7863d403ac81fb2d15309d331223c74092d
Syotakokichi <kokichiyam@gmail.com>
2023-08-02 21:01:09 +0900

    puma変sy封

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-unix:///var/www/backend/tmp/sockets/puma.sock
+bind "unix:///var/www/backend/tmp/sockets/puma.sock"
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
